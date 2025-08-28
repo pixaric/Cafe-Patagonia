13e11c74ba0f279280e10706d9057c0f12a50cef
reinier <reiniercg2020@gmail.com>
2025-08-28 13:24:26 +0100

    parece q ya

--- firebase-config.js
+++ firebase-config.js
@@ -8,7 +8,9 @@ const firebaseConfig = {
   projectId: "pedidosqr-34bea",
   storageBucket: "pedidosqr-34bea.firebasestorage.app",
   messagingSenderId: "743534119824",
-  appId: "1:743534119824:web:17cf79d3b6952380084115"
+  appId: "1:743534119824:web:17cf79d3b6952380084115",
+  databaseURL: "https://pedidosqr-34bea-default-rtdb.europe-west1.firebasedatabase.app"
+
 };
 
 const app = initializeApp(firebaseConfig);
